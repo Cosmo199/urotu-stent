8f671b9b2b9f5fab03a71c70ac1a2125d942e208
nawakarn <karn939.n@gmail.com>
2022-01-04 23:14:53 +0700

    integrate cloud-messaging

---
+++ app/src/main/java/com/example/medicomgmester/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.example.medicomgmester
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d("msg", "Message data payload: " + remoteMessage.data)
+    }
+}
